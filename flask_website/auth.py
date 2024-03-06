d1ee9d84256b3023e1e1d19952b4e4d5b8a6f895
JieYi-777 <limjieyi777@gmail.com>
2024-03-06 15:58:33 +0800

    Clean and comemnt the auth.py

--- flask_website/auth.py
+++ flask_website/auth.py
@@ -7,11 +7,11 @@ import logging
 # Create Blueprint object
 auth = Blueprint('auth', __name__)
 
-# Get a logger
+# Get a logger for logging
 logger = logging.getLogger(__name__)
 
 
-# Route to serve the user registration
+# Route to serve the user registration and return the token with message
 @auth.route('/register', methods=['POST'])
 def register():
     try:
@@ -19,7 +19,7 @@ def register():
         # Get registration form data in json format
         user_data = request.json
 
-        # Get each data
+        # Get each data from request
         username = user_data.get('username')
         email = user_data.get('email')
         password = user_data.get('password')
@@ -34,10 +34,10 @@ def register():
         elif existing_email:
             return jsonify({'message': 'Email already exists'}), 400
 
-        # Create new user object and store in database
+        # Create new user object with username, email and encrypted password
         new_user = User(username=username, email=email, password_hash=bcrypt.generate_password_hash(password))
 
-        # Create welcome message
+        # Create welcome message object, reference to the new user
         welcome_message = Notification(title='Welcome to Smart Finance!', message="Welcome to Smart Finance! We're "
                                                                                   "thrilled to have you join our "
                                                                                   "community. Here, you can easily manage "
@@ -54,62 +54,65 @@ def register():
                                                                                   "questions. Happy budgeting!",
                                        user=new_user)
 
+        # Store the new user and message in database, committing the transactions
         db.session.add(new_user)
         db.session.add(welcome_message)
         db.session.commit()
 
+        # Create the JWT token
         access_token = create_access_token(identity=new_user.id)
 
+        # Return the message with the token
         return jsonify({'message': 'Account created successfully', 'token': access_token}), 201
 
     except Exception as e:
         # Rollback changes if an error occurs
         db.session.rollback()
+
+        # Logging the error in console and return the error message to frontend
         logger.error(f'An error occurred: {str(e)}')
         return jsonify({'message': 'An error occurred while registering account'}), 500
 
 
-# Route to serve the user login
+# Route to serve the user login and return the token with message
 @auth.route('/login', methods=['POST'])
 def login():
     try:
         # Get user login data in json format
         user_data = request.json
 
-        # Get each data
+        # Get each data from the request
         identifier = user_data.get('identifier')
         password = user_data.get('password')
         is_email = True if user_data.get('isEmail') == 'true' else False
 
+        # If it is email, search the user using the email, else using the username
         if is_email:
             user = User.query.filter_by(email=identifier).first()
         else:
             user = User.query.filter_by(username=identifier).first()
 
+        # If the user is registered and found in database, check the password, else return user not found error message
         if user:
+            # If the password is correct, then create the token, return it with message to user
             if bcrypt.check_password_hash(user.password_hash, password):
                 access_token = create_access_token(identity=user.id)
 
                 return jsonify({'message': 'Login successful', 'token': access_token}), 200
+
+            # Else, return the password error message
             else:
                 return jsonify({'message': 'Incorrect password'}), 401
         else:
             return jsonify({'message': 'User not found'}), 404
 
+    # If any error, logging the message and return the message to user
     except Exception as e:
         logger.error(f'An error occurred: {str(e)}')
         return jsonify({'message': 'An error occurred while logging in'}), 500
 
 
 # Route to serve the user logout
-@auth.route('/logout')
-def logout():
-    return jsonify({'message': 'User logged out successfully'})
-
-@auth.route('/test')
-@jwt_required()
-def test():
-    current_user = get_jwt_identity()
-    print(current_user)
-
-    return jsonify({'message': 'Test good'})
+# @auth.route('/logout')
+# def logout():
+#     return jsonify({'message': 'User logged out successfully'})
