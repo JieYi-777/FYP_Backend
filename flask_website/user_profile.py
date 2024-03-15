873a6a3db4e7b4648f72e14f90b8e5614f90e3df
JieYi-777 <limjieyi777@gmail.com>
2024-03-15 21:34:08 +0800

    Change Password

--- flask_website/user_profile.py
+++ flask_website/user_profile.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from .models import User
-from . import db
+from . import bcrypt, db
 import logging
 
 # Create Blueprint object
@@ -89,3 +89,45 @@ def update_email():
 
         logger.error(e)
         return jsonify({'message': 'An error occurred while updating email.'}), 500
+
+
+@user_profile.route('/change-password', methods=['PUT'])
+@jwt_required()
+def change_password():
+    try:
+
+        # Get the current user id
+        user_id = get_jwt_identity()
+
+        # Retrieve the user object from the database
+        user = User.query.get(user_id)
+
+        # Get the old password and new password from the request
+        password_data = request.json
+        old_password = password_data.get('old_password')
+        new_password = password_data.get('new_password')
+
+        # Check the user is existed or not
+        if user:
+            # If the password is correct, then change the password, return it with message to user
+            if bcrypt.check_password_hash(user.password_hash, old_password):
+
+                user.password_hash = bcrypt.generate_password_hash(new_password)
+
+                # Save changes to the database
+                db.session.commit()
+
+                return jsonify({'message': 'Password changed successfully.'}), 200
+
+            # Else, return the password error message
+            else:
+                return jsonify({'message': 'Incorrect Old Password'}), 401
+        else:
+            return jsonify({'message': 'Account Not Found'}), 404
+
+    except Exception as e:
+        # Rollback changes if an error occurs
+        db.session.rollback()
+
+        logger.error(e)
+        return jsonify({'message': 'An error occurred while changing password.'}), 500
