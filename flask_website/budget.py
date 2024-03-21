c6232252b750a8d1e6a319035ce2af4f28363496
JieYi-777 <limjieyi777@gmail.com>
2024-03-21 16:30:35 +0800

    Add budget

--- flask_website/budget.py
+++ flask_website/budget.py
@@ -10,6 +10,7 @@ budget = Blueprint('budget', __name__)
 # Get a logger for logging
 logger = logging.getLogger(__name__)
 
+
 # To get all the budgets related to the user
 @budget.route('')
 @jwt_required()
@@ -45,3 +46,35 @@ def get_all_budgets():
     except Exception as e:
         logger.error(e)
         return jsonify({'message': str(e)}), 500
+
+
+# To add budget
+@budget.route('/add-budget', methods=['POST'])
+@jwt_required()
+def add_budget():
+    try:
+        # Get the current user id
+        user_id = get_jwt_identity()
+
+        # Get budget data to be store from request
+        budget_data = request.json
+
+        # Get each data from the request
+        category_id = budget_data.get('category_id')
+        amount = budget_data.get('amount')
+
+        # Create new budget object
+        new_budget = Budget(amount=amount, category_id=category_id, user_id=user_id)
+
+        # Store the new budget in database, committing the transactions
+        db.session.add(new_budget)
+        db.session.commit()
+
+        return jsonify({'message': 'The budget has been successfully added.'}), 200
+
+    except Exception as e:
+        # Rollback changes if an error occurs
+        db.session.rollback()
+
+        logger.error(e)
+        return jsonify({'message': 'An error occurred while adding budget.'}), 500
