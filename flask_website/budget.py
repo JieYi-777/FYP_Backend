817a34f0cb3a1738b0e1f3093244fb090058ad42
JieYi-777 <limjieyi777@gmail.com>
2024-03-22 12:04:18 +0800

    Update budget and Delete budget

--- flask_website/budget.py
+++ flask_website/budget.py
@@ -78,3 +78,69 @@ def add_budget():
 
         logger.error(e)
         return jsonify({'message': 'An error occurred while adding budget.'}), 500
+
+
+# To update the budget in database
+@budget.route('/update-budget/<budget_id>', methods=['PUT'])
+@jwt_required()
+def update_budget(budget_id):
+    try:
+
+        # Get the current user id
+        user_id = get_jwt_identity()
+
+        # Get budget data to be store from request
+        budget_data = request.json
+
+        # Get each data from the request
+        category_id = budget_data.get('category_id')
+        amount = budget_data.get('amount')
+
+        # Update the budget
+        updated_budget = Budget.query.filter_by(id=budget_id, user_id=user_id).update(
+            {
+                'category_id': category_id,
+                'amount': amount
+            }
+        )
+
+        # Commit the changes to the database
+        db.session.commit()
+
+        if updated_budget:
+            return jsonify({'message': "Budget updated successfully."}), 200
+        else:
+            return jsonify({'message': "No Budget Found"}), 404
+
+    except Exception as e:
+        db.session.rollback()
+
+        logger.error(e)
+        return jsonify({'message': 'An error occurred while updating budget.'}), 500
+
+
+# To delete the budget
+@budget.route('/delete-budget/<budget_id>', methods=['DELETE'])
+@jwt_required()
+def delete_budget(budget_id):
+    try:
+
+        # Get the current user id
+        user_id = get_jwt_identity()
+
+        # Delete the budget directly using a query
+        deleted_budget = Budget.query.filter_by(id=budget_id, user_id=user_id).delete()
+
+        # Commit the changes to the database
+        db.session.commit()
+
+        if deleted_budget:
+            return jsonify({'message': 'Budget deleted successfully.'}), 200
+        else:
+            return jsonify({'message': 'Budget Not Found'}), 404
+
+    except Exception as e:
+        db.session.rollback()
+
+        logger.error(e)
+        return jsonify({'message': 'An error occurred while deleting budget.'}), 500
